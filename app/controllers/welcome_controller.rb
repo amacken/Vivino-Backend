ec3d18d56540666c78c82b6fed3fa738d264afd1
amacken <alex.mackenzie2@gmail.com>
2020-09-24 16:20:41 -0400

    testing if wines show up

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
     def index
-        render json: { status: 200, message: "Wine-O API" }
+        # render json: { status: 200, message: "Wine-O API" }
+        @wines = Wine.all.reverse
+
+        render json: @wines
     end
 end
